76d6600f40f880505be75164835e542ddce66aca
Matis <matis.masters@neonroots.com>
2012-12-01 23:01:33 +0100

    Informations index

---
+++ app/controllers/informations.rb
@@ -0,0 +1,27 @@
+Merobaron.controllers :informations do
+  get :index, map: "/informations" do
+    @informations = Information.order(:title)
+    haml :'informations/index'
+  end
+
+  # get :index, :map => "/foo/bar" do
+  #   session[:foo] = "bar"
+  #   render 'index'
+  # end
+
+  # get :sample, :map => "/sample/url", :provides => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   "Maps to url '/foo/#{params[:id]}'"
+  # end
+
+  # get "/example" do
+  #   "Hello world!"
+  # end
+
+  
+end
