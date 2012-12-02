791eb03acd750d168398e1d8ad9f71f1e5219726
Matis <matis.masters@neonroots.com>
2012-12-02 07:26:17 -0200

    API basic functions, added json plugin for sequel
    By date only

--- app/controllers/submissions.rb
+++ app/controllers/submissions.rb
@@ -1,3 +1,4 @@
+require 'json'
 Merobaron.controllers :submissions do
   post :create, "/submissions" do
     @submission = Submission.new(params[:submission])
@@ -15,4 +16,29 @@ Merobaron.controllers :submissions do
       "NOK"
     end
   end
+
+  get :submissions_by_station, "/submissions/:station_number/:start_date/:end_date" do
+    if params[:station_number].blank? || params[:station_number].to_i > 20 || params[:station_number].to_i < 1
+      status 403
+      return "The station number is not valid or not present"
+    end
+    if params[:start_date].blank? || params[:end_date].blank?
+      status 403
+      return "Either the start or end date is not properly formated or not present"
+    end
+    
+    @submissions = Submission.by_station(params[:station_number], params[:start_date].to_time, params[:end_date].to_time)
+    @submissions.to_json
+  end
+
+  get :submissions_by_date, "/submissions/:start_date/:end_date" do
+    if params[:start_date].blank? || params[:end_date].blank?
+      status 403
+      return "Either the start or end date is not properly formated or not present"
+    end
+    
+    @submissions = Submission.by_date(params[:start_date].to_time, params[:end_date].to_time)
+    @submissions.to_json
+  end
+
 end
