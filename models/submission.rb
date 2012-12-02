35dcdef6a60c2c7fb14b27de7e9eb901f909d2fe
Oscar Siniscalchi <oscar.siniscalchi@neonroots.com>
2012-12-02 12:47:02 -0300

    WIP map stats

--- models/submission.rb
+++ models/submission.rb
@@ -2,14 +2,14 @@ class Submission < Sequel::Model
   many_to_many :items
 
   def self.by_station(station_number, start_date, end_date)
-    where( station_number: station_number, happend_at: start_date..end_date ).all
+    where( station_number: station_number, happened_at: start_date..end_date ).all
   end
 
   def self.by_date(start_date, end_date)
-    where( happend_at: start_date..end_date ).all
+    where( happened_at: start_date..end_date ).all
   end
 
-  def to_json(opts)
+  def to_json(opts = {})
     opts.merge( {:include => { items: { only: label } } } )
     super(opts)
   end
--- public/javascripts/map.widget.js
+++ public/javascripts/map.widget.js
@@ -74,10 +74,100 @@ $.widget("meRobaron.mapWidget", {
           });
 
         }
+      },
+      getRandomColor: function(){
+        var col =self.colors[Math.floor(Math.random() * (3 - 1)) + 0];
+        console.log(col);
+        return col;
+      },
+
+});
+
+$.widget("meRobaron.mapStatsWidget", {
+      _init: function(){
+
+        self = this;
+        this.mapWrapper = $("#map-wrapper");
+        this.start_date = $(".datepicker.start");
+        this.end_date = $(".datepicker.end");
+        
+        $('.datepicker').datepicker({
+          dateFormat: "yy-mm-dd",
+        });
+
+        $("#controls .btn.refresh").click(function(){
+             var url = "/submissions/"+ self.start_date.val() +"/" + self.end_date.val();
+             $.get(url,{}, function(data){
+                  self.reloadData(data);
+             },'json');
+        })
+
+        var center = new google.maps.LatLng(-34.886952,-56.126281);
+        var mapOptions = {
+          zoom: 12,
+          center: center,
+          mapTypeId: google.maps.MapTypeId.ROADMAP,
+          disableDefaultUI: true,
+          panControl: false,
+          zoomControl: true,
+          mapTypeControl: false,
+          scaleControl: false,
+          streetViewControl: false,
+          overviewMapControl: false
+        };
+
+        this.map = new google.maps.Map(document.getElementById("map-wrapper"),
+            mapOptions);
+
+       for ( idx in seccionales.Document.Folder.Placemark ){
+          var seccional = seccionales.Document.Folder.Placemark[idx];
+          var seccionalNumber = seccional.Polygon.outerBoundaryIs.LinearRing.seccionalNumber;
+          var seccionalChords = [];
+
+          for (chord_idx in seccional.Polygon.outerBoundaryIs.LinearRing.coordinates){
+              var chord = seccional.Polygon.outerBoundaryIs.LinearRing.coordinates[chord_idx];
+
+              var g_chord = new google.maps.LatLng(chord[1], chord[0]);
+              seccionalChords.push(g_chord);
+          }
+
+          seccionalPolygon = new google.maps.Polygon({
+            paths: seccionalChords,
+            title: seccionalNumber,
+            strokeColor: "#fe3455",
+            strokeOpacity: 0.9,
+            strokeWeight: 1,
+            fillColor: "#ef7884",
+            fillOpacity: 0.5
+          });
+
+          seccionalPolygon.setMap(self.map); 
+          google.maps.event.addListener(seccionalPolygon, 'click', function( event ){
+              
+              var url = "/submissions/"+ this.title +"/"+ self.start_date.val() +"/" + self.end_date.val();
+              $.get(url,{}, function(data){
+                 self.reloadData(data);
+              },'json');
+
+          });
+
+        }
 
 
         google.maps.event.addListener(this.map, 'click', function( event ){
           });
+      },
+
+      reloadData: function(data){
+          for (i in data){
+              if(data[i].lat && data[i]long){
+                var latlang = +event.latLng.lat()+","+event.latLng.lng(); 
+                // To add the marker to the map, call setMap();
+                marker.setMap(self.map);
+                self.marker = marker;
+              }
+          }
+          console.log(data);
       },
       getRandomColor: function(){
         var col =self.colors[Math.floor(Math.random() * (3 - 1)) + 0];
