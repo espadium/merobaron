41eda866f83442b1cf5d34e671463b6af4613538
Matis <matis.masters@neonroots.com>
2012-12-02 05:01:06 -0200

    Added submissions controller

---
+++ app/controllers/submissions.rb
@@ -0,0 +1,18 @@
+Merobaron.controllers :submissions do
+  post :create, "/submissions" do
+    @submission = Submission.new(params[:submission])
+    
+    if @submission.save rescue false
+      items = params.select! { |key, val| key if key.to_s.include?("item_") && !val.blank? }
+        
+      items.each do |key, item_id|
+        item = Item[item_id]
+        @submission.add_item(item) unless item.nil?
+        @submission.save
+      end
+      "OK"
+    else
+      "NOK"
+    end
+  end
+end
