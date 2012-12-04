035332c940f17b40d95e5d92105e372794f95793
Matis <matis.masters@neonroots.com>
2012-12-04 10:10:39 -0200

    Fixed to_json problem

--- models/submission.rb
+++ models/submission.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class Submission < Sequel::Model
   many_to_many :items
 
@@ -9,16 +11,25 @@ class Submission < Sequel::Model
     where( happened_at: start_date..end_date ).all
   end
 
-  def to_json(opts = {})
-    opts.merge( {:include => { items: { only: :label } } } )
-    super(opts)
-  end
-
   def has_item?(item_name)
     items.map(&:name).includes?(item_name)
   end
 
   def informations
     self.items.map(&:informations).flatten
+  end
+
+  def to_json
+    {
+      lat: lat,
+      long: long,
+      statation_number: station_number,
+      hapepend_at: happened_at,
+      items: items_to_json
+    }
+  end
+
+  def items_to_json
+    items.map { |i| { title: i.title, name: i.name, label: i.label, suggestion: i.suggestion } }
   end
 end
