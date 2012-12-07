40fe96bc166462dc02b68196f500ba60d989cb6b
Mojo <santiago.mollajoli@neonroots.com>
2012-12-07 18:46:56 -0200

    Ribons

--- config/database.rb
+++ config/database.rb
@@ -2,7 +2,7 @@ Sequel::Model.plugin(:schema)
 Sequel::Model.plugin :json_serializer
 Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
 Sequel::Model.db = case Padrino.env
-  when :development then Sequel.connect("postgres://admin:postgres@localhost/merobaron_development", :loggers => [logger])
+  when :development then Sequel.connect("postgres://localhost/merobaron_development", :loggers => [logger])
   when :production  then Sequel.connect(ENV['DATABASE_URL'],  :loggers => [logger])
   when :test        then Sequel.connect("postgres://admin:postgres@localhost/merobaron_test",  :loggers => [logger])
 end
